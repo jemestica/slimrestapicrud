80f568bf6bb5b2475b0c893d6fe76ddf5b95a9e1
Julio Villalta <julio@bytto.com>
2017-06-28 19:45:40 -0600

    Estructura

---
+++ app/route/user_route.php
@@ -0,0 +1,10 @@
+<?php
+
+$app->group('/user/', function () {
+    
+    $this->get('test', function ($req, $res, $args) {
+        return $res->getBody()
+                   ->write('Hello Users');
+    });
+    
+});
